e7e97b3ce790d3f0753ee95f349ffc07a0880c38
Cercva1 <giorgi.tsertsvadze@sa.stud.vu.lt>
2025-06-04 13:52:57 +0400

    Contains the shared data model used for passing word count data between agents and the master.

--- ConsoleApp1/Common/WordIndexEntry.cs
+++ ConsoleApp1/Common/WordIndexEntry.cs
@@ -1,6 +1,15 @@
-﻿namespace Common;
+﻿using system;
 
-public class Class1
-{
+namespace Common
+{   
+    [Serializable]
+    public class WordIndexEntry
+    {
+        public string FileName { get; set; }
+        public string Word { get; set; }
+        public int Count { get; set; }
 
+    }   
+    
 }
+
