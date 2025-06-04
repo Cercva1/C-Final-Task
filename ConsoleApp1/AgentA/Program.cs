862fca803375ced0c879fb8d9e0789ab9dd4501c
Cercva1 <giorgi.tsertsvadze@sa.stud.vu.lt>
2025-06-04 14:04:33 +0400

    Now we start implementing and source of code in AgentA and we start doing with filescaner.FileScaner is the class that does the actual work of reading and analyzing the text files in the directory for Agent.

--- ConsoleApp1/AgentA/Program.cs
+++ ConsoleApp1/AgentA/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
+using Common;
+
+namespace AgentA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set CPU affinity (e.g., core 0 for AgentA)
+
+        }
+
+         static void FileScanner(string dirPath, ConcurrentQueue<WordIndexEntry> queue)
+        {
+            foreach (var file in Directory.GetFiles(dirPath, "*.txt"))
+            {
+                var wordCount = new ConcurrentDictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+                foreach (var line in File.ReadLines(file))
+                {
+                    foreach (var word in line.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        wordCount.AddOrUpdate(word, 1, (k, v) => v + 1);
+                    }
+                }
+                foreach (var kvp in wordCount)
+                {
+                    queue.Enqueue(new WordIndexEntry
+                    {
+                        FileName = Path.GetFileName(file),
+                        Word = kvp.Key,
+                        Count = kvp.Value
+                    });
+                }
+            }
+        }
+
+    }
+}
